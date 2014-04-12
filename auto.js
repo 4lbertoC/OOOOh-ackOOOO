c44358c096b71ff49eb4e85637854cdc61e0f032
4lbertoC <4lbertoc@gmail.com>
2014-04-12 22:42:26 +0200

    UUUHAAAAAAAARRRGHHHHgit statusgit status

--- auto.js
+++ auto.js
@@ -3,28 +3,40 @@
 	var _carX = 45.474;
 	var _carY = 9.186;
 	var _map;
+	var _mapCanvas = document.getElementById("map-canvas");
+	var _marker;
+	var _position;
 	var serverurl = "http://smartcities.herokuapp.com/positions"
 
+	function onPosition(data) {
+		var latLng = new google.maps.LatLng(data.lat, data.lng);
+		_marker.setPosition(latLng);
+		_map.setOptions({
+			center: latLng
+		});
+	}
+
 	function initialize() {
+		var _position = new google.maps.LatLng(_carX, _carY);
 	    var mapOptions = {
-	        center: new google.maps.LatLng(_carX, _carY),
+	        center: _position,
 	        zoom: 15
 	    };
 	    _map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
+	    _marker = new google.maps.Marker({
+	    	position: _position,
+	    	map: _map
+	    });
+
 		google.maps.event.addListener(_map, "click", function(event) {
 		    var lat = event.latLng.lat();
 		    var lng = event.latLng.lng();
-		    // populate yor box/field with lat, lng
-		    //alert("Lat=" + lat + "; Lng=" + lng);
 			$.ajax({
 			  type: "POST",
 			  url: serverurl,
 			  data: '{"lat": "' + lat + '", "lng" : "' + lng + '"}',
-			  success: function (data) {
-			  	//alert(data);
-			  	console.log(data);
-			  },
+			  success: onPosition,
 			  contentType: 'json',
 			  xhrFields: {
 			      withCredentials: false
@@ -35,14 +47,12 @@
 	}
 	google.maps.event.addDomListener(window, 'load', initialize);
 
-	function onPosition(position) {
-
-	}
-
 	function init() {
+		$(_mapCanvas).css("visibility", "visible");
 		$('.loading').remove();
-	}
 
+		AudiHack.getPosition(onPosition);
+	}
 
 	init();
 })();
