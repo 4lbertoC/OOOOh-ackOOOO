686672fba3998409ef28835cb99cfc301f365e0e
4lbertoC <4lbertoc@gmail.com>
2014-04-12 20:46:50 +0200

    added other stuff

--- main.js
+++ main.js
@@ -1,17 +1,41 @@
 (function() {
 
+	var _map;
+	var _carX = 45.474;
+	var _carY = 9.186;
+	var _position = new google.maps.LatLng(_carX, _carY);
+
 	function initialize() {
 	    var mapOptions = {
-	        center: new google.maps.LatLng(-34.397, 150.644),
-	        zoom: 8
+	        center: ,
+	        zoom: 15
 	    };
-	    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+	    map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+		google.maps.event.addListener(map, "click", function(event) {
+		    var lat = event.latLng.lat();
+		    var lng = event.latLng.lng();
+		    // populate yor box/field with lat, lng
+		    alert("Lat=" + lat + "; Lng=" + lng);
+		});
 	}
-	// google.maps.event.addDomListener(window, 'load', initialize);
+	google.maps.event.addDomListener(window, 'load', initialize);
+
+	function onPosition(position) {
+
+	}
 
 	function init() {
-		
+		$('.loading').remove();
+
+		setInterval(function() {
+			_carX += 1;
+			map.setOptions({
+				center: new goo
+			})
+		}, 1000);
 	}
 
 
+	init();
 })();
